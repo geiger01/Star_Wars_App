6222a0c0d2c33d8c4c114401b9689cc4285fb47c
Jonathan Geiger <geiger01@Jonathans-MBP.lan>
2021-10-25 12:21:32 +0300

    Project file structure completed, including sass and  components

---
+++ src/components/MoviePreview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MoviePreview = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/pages/MovieApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MovieApp = () => {
+    return (
+        <main className="movie-app">
+            <h1>Movie App</h1>
+        </main>
+    )
+}
